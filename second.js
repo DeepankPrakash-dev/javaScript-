2adc7ec6d9749613b67b517002451809b3d7f9ad
DeepankPrakash-dev <deepank.prakash@rtctek.com>
2026-01-13 17:28:54 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,13 @@
+let employee = {
+    name : "deepank",
+    age : 24,
+    company : "RTC",
+    is_active : true,
+    is_intern : false,
+    "working hours" : "9 to 5",
+    salary : "50LAKH"
+
+}
+
+console.log(employee);
+(employee).is_intern = true;
